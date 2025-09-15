087868799e225040b9704aa73286eaa9284d7728
Dave <121976848+IamDaveee@users.noreply.github.com>
2025-09-15 08:37:49 +0200

    Add files via upload

---
+++ Class_Gyak03/Dolgozok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Gyak03
+{
+    class Dolgozok
+    {
+        public string nev, munkakör;
+        public int fizetes;
+
+        public Dolgozok(string nev, string munkakör, int fizetes)
+        {
+            this.nev = nev;
+            this.munkakör = munkakör;
+            this.fizetes = fizetes;
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"Dolgozó neve: {nev}, Munkaköre: {munkakör}, Fizetése: {fizetes}");
+        }
+
+        public int Emel()
+        {
+            return fizetes + 100000;
+        }
+    }
+}
---
+++ Class_Gyak03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Gyak03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolgozok dolgozo1 = new Dolgozok("Ede", "takarító", 250000);
+
+            dolgozo1.Kiir();
+            Console.WriteLine($"A dolgozó új fizetése: {dolgozo1.Emel()}");
+        }
+    }
+}
