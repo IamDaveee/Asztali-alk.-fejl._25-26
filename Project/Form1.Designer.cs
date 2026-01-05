2578f1929e70498332cb26ea6a8121def6224627
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-01-05 13:05:32 +0100

    Add files via upload

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿
+namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btdDesc = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.szabalyLeiras = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNote1 = new System.Windows.Forms.TextBox();
+            this.btnNoteForward = new System.Windows.Forms.Button();
+            this.btnNoteBack = new System.Windows.Forms.Button();
+            this.txtNote2 = new System.Windows.Forms.TextBox();
+            this.btnNoteExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(345, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Üdvözlünk a programban";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(121, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Játék Kezdése";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(123, 229);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(642, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Leírás";
+            // 
+            // btdDesc
+            // 
+            this.btdDesc.Location = new System.Drawing.Point(623, 229);
+            this.btdDesc.Name = "btdDesc";
+            this.btdDesc.Size = new System.Drawing.Size(85, 23);
+            this.btdDesc.TabIndex = 4;
+            this.btdDesc.Text = "Játékszabály";
+            this.btdDesc.UseVisualStyleBackColor = true;
+            this.btdDesc.Click += new System.EventHandler(this.btdDesc_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(763, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(37, 21);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(370, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Játék neve";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(354, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Játékszabály:";
+            this.label5.Visible = false;
+            // 
+            // szabalyLeiras
+            // 
+            this.szabalyLeiras.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szabalyLeiras.Location = new System.Drawing.Point(256, 122);
+            this.szabalyLeiras.Multiline = true;
+            this.szabalyLeiras.Name = "szabalyLeiras";
+            this.szabalyLeiras.ReadOnly = true;
+            this.szabalyLeiras.Size = new System.Drawing.Size(307, 293);
+            this.szabalyLeiras.TabIndex = 8;
+            this.szabalyLeiras.Text = resources.GetString("szabalyLeiras.Text");
+            this.szabalyLeiras.Visible = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(124, 392);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "Vissza";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Visible = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnNoteExit);
+            this.panel1.Controls.Add(this.btnNoteBack);
+            this.panel1.Controls.Add(this.btnNoteForward);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lblNotes);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 460);
+            this.panel1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(269, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(217, 32);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Kiinduló helyzet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(242, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(296, 372);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(544, 415);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Tovább";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Location = new System.Drawing.Point(163, 171);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(55, 13);
+            this.lblNotes.TabIndex = 5;
+            this.lblNotes.Text = "Jegyzetek";
+            this.lblNotes.Visible = false;
+            this.lblNotes.Click += new System.EventHandler(this.lblNotes_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(242, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 372);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.txtNote2);
+            this.panel2.Controls.Add(this.txtNote1);
+            this.panel2.Location = new System.Drawing.Point(327, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(107, 244);
+            this.panel2.TabIndex = 7;
+            this.panel2.Visible = false;
+            // 
+            // txtNote1
+            // 
+            this.txtNote1.BackColor = System.Drawing.Color.White;
+            this.txtNote1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNote1.Location = new System.Drawing.Point(0, 0);
+            this.txtNote1.Multiline = true;
+            this.txtNote1.Name = "txtNote1";
+            this.txtNote1.ReadOnly = true;
+            this.txtNote1.Size = new System.Drawing.Size(107, 244);
+            this.txtNote1.TabIndex = 0;
+            this.txtNote1.Text = "Jegyzetek a jegyzetfüzetben nagyon picivel írva";
+            // 
+            // btnNoteForward
+            // 
+            this.btnNoteForward.Location = new System.Drawing.Point(544, 415);
+            this.btnNoteForward.Name = "btnNoteForward";
+            this.btnNoteForward.Size = new System.Drawing.Size(75, 23);
+            this.btnNoteForward.TabIndex = 8;
+            this.btnNoteForward.Text = "Tovább";
+            this.btnNoteForward.UseVisualStyleBackColor = true;
+            this.btnNoteForward.Visible = false;
+            this.btnNoteForward.Click += new System.EventHandler(this.btnNoteForward_Click);
+            // 
+            // btnNoteBack
+            // 
+            this.btnNoteBack.Location = new System.Drawing.Point(161, 415);
+            this.btnNoteBack.Name = "btnNoteBack";
+            this.btnNoteBack.Size = new System.Drawing.Size(75, 23);
+            this.btnNoteBack.TabIndex = 9;
+            this.btnNoteBack.Text = "Vissza";
+            this.btnNoteBack.UseVisualStyleBackColor = true;
+            this.btnNoteBack.Visible = false;
+            this.btnNoteBack.Click += new System.EventHandler(this.btnNoteBack_Click);
+            // 
+            // txtNote2
+            // 
+            this.txtNote2.BackColor = System.Drawing.Color.White;
+            this.txtNote2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNote2.Location = new System.Drawing.Point(0, 0);
+            this.txtNote2.Multiline = true;
+            this.txtNote2.Name = "txtNote2";
+            this.txtNote2.ReadOnly = true;
+            this.txtNote2.Size = new System.Drawing.Size(107, 244);
+            this.txtNote2.TabIndex = 1;
+            this.txtNote2.Text = "Jegyzetfüzet második oldala";
+            this.txtNote2.Visible = false;
+            // 
+            // btnNoteExit
+            // 
+            this.btnNoteExit.Location = new System.Drawing.Point(161, 415);
+            this.btnNoteExit.Name = "btnNoteExit";
+            this.btnNoteExit.Size = new System.Drawing.Size(75, 23);
+            this.btnNoteExit.TabIndex = 10;
+            this.btnNoteExit.Text = "Kilép";
+            this.btnNoteExit.UseVisualStyleBackColor = true;
+            this.btnNoteExit.Visible = false;
+            this.btnNoteExit.Click += new System.EventHandler(this.btnNoteExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.szabalyLeiras);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btdDesc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btdDesc;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox szabalyLeiras;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtNote1;
+        private System.Windows.Forms.Button btnNoteBack;
+        private System.Windows.Forms.Button btnNoteForward;
+        private System.Windows.Forms.TextBox txtNote2;
+        private System.Windows.Forms.Button btnNoteExit;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btdDesc_Click(object sender, EventArgs e)
+        {
+            btnStart.Visible = false;
+            btdDesc.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+
+            label5.Visible = true;
+            szabalyLeiras.Visible = true;
+            btnBack.Visible = true;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            btnStart.Visible = true;
+            btdDesc.Visible =  true;
+            label1.Visible =   true;
+            label2.Visible =   true;
+            label3.Visible =   true;
+            label4.Visible = true;
+
+            label5.Visible = false;
+            szabalyLeiras.Visible = false;
+            btnBack.Visible = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            label6.Text = "Informatika terem";
+            btnNext.Visible = false;
+            lblNotes.Visible = true;
+        }
+
+        private void lblNotes_Click(object sender, EventArgs e)
+        {
+            label6.Text = "Jegyzetek";
+            pictureBox1.Image = Image.FromFile("notepad.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Visible = true;
+            panel2.Visible = true;
+            panel2.BringToFront();
+            btnNoteForward.Visible = true;
+            btnNoteExit.Visible = true;
+        }
+
+        private void btnNoteForward_Click(object sender, EventArgs e)
+        {
+            btnNoteExit.Visible = false;
+            btnNoteBack.Visible = true;
+            btnNoteForward.Visible = false;
+            txtNote1.Visible = false;
+            txtNote2.Visible = true;
+        }
+
+        private void btnNoteBack_Click(object sender, EventArgs e)
+        {
+            btnNoteBack.Visible = false;
+            btnNoteExit.Visible = true;
+            btnNoteForward.Visible = true;
+            txtNote1.Visible = true;
+            txtNote2.Visible = false;
+        }
+
+        private void btnNoteExit_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            pictureBox1.Visible = false;
+            btnNoteForward.Visible = false;
+            btnNoteExit.Visible = false;
+            label6.Text = "Informatika Terem";
+        }
+    }
+}
