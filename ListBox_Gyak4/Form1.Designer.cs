6d2e8019ba392496eb03360246b99bcf98445451
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-01-26 11:25:40 +0100

    Add files via upload

---
+++ ListBox_Gyak4/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace ListBox_Gyak4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnSmall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(168, 111);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Hozzáad";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(168, 85);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(301, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(454, 82);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Törlés";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Location = new System.Drawing.Point(454, 111);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(75, 39);
+            this.btnDeleteAll.TabIndex = 4;
+            this.btnDeleteAll.Text = "Összes Törlése";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(168, 141);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Feltölt";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(298, 194);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 6;
+            this.lblCount.Text = "label1";
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(454, 157);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(75, 23);
+            this.btnDouble.TabIndex = 7;
+            this.btnDouble.Text = "Dupla";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(454, 187);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Számol";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnSmall
+            // 
+            this.btnSmall.Location = new System.Drawing.Point(454, 217);
+            this.btnSmall.Name = "btnSmall";
+            this.btnSmall.Size = new System.Drawing.Size(75, 23);
+            this.btnSmall.TabIndex = 9;
+            this.btnSmall.Text = "Legkisebb";
+            this.btnSmall.UseVisualStyleBackColor = true;
+            this.btnSmall.Click += new System.EventHandler(this.btnSmall_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSmall);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnSmall;
+    }
+}
+
---
+++ ListBox_Gyak4/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBox_Gyak4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void CountChanges()
+        {
+            lblCount.Text = Convert.ToString(listBox1.Items.Count);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Balázs Dávid";
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(rnd.Next(100, 1000));
+            }
+            lblCount.Text = "";
+            CountChanges();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtBox1.Text=="")
+            {
+                MessageBox.Show("Üres mező", "Figyelem");
+            }
+            else
+            {
+                listBox1.Items.Add(Convert.ToString(txtBox1.Text));
+            }
+            txtBox1.Text = "";
+            CountChanges();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            CountChanges();
+        }
+
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            CountChanges();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                listBox1.Items.Add(rnd.Next(100, 1000));
+            }
+            CountChanges();
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            int countDarab = 0;
+            int countRemoved = 0;
+            int listLength = listBox1.Items.Count;
+
+            for (int i = 0; i < listLength; i++)
+            {
+                for (int j = 0; j < listLength; j++)
+                {
+                    if (Convert.ToString(listBox1.Items[j]) == Convert.ToString(listBox1.Items[i]))
+                    {
+                        countDarab++;
+                        if (countDarab >= 2)
+                        {
+                            listBox1.Items.RemoveAt(listBox1.Items.IndexOf(listBox1.Items[j]));
+                            countRemoved++;
+                            countDarab--;
+                            j--;
+                            listLength--;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                countDarab = 0;
+            }
+            CountChanges();
+            MessageBox.Show($"Kivett elemek száma: {countRemoved}");
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            List<int> Szamok = new List<int>();
+            foreach (var item in listBox1.Items)
+            {
+                Szamok.Add(Convert.ToInt32(item));
+            }
+            MessageBox.Show($"Összeg: {Szamok.Sum()}\nÁtlag: {Szamok.Average():0.00}");
+        }
+
+        private void btnSmall_Click(object sender, EventArgs e)
+        {
+            List<int> Szamok = new List<int>();
+            foreach (var item in listBox1.Items)
+            {
+                Szamok.Add(Convert.ToInt32(item));
+            }
+            listBox1.SetSelected(Convert.ToInt32(Szamok.IndexOf(Szamok.Min())), true);
+        }
+    }
+}
