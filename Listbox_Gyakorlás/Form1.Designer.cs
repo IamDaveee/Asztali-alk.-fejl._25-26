9179c2046ed86b9d071fc530628032bd8b9dc2be
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-02-02 10:45:01 +0100

    Add files via upload

---
+++ Listbox_Gyakorlás/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace Listbox_Gyakorlás
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnComboAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFullDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(90, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(275, 168);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Páros";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(90, 320);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Feltölt";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(275, 320);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(100, 23);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Páros elemek";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Elemek száma:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(275, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Elemek száma: ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(275, 99);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Hozzáadás";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(407, 100);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Keresés";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alma",
+            "Barack",
+            "Körte"});
+            this.comboBox1.Location = new System.Drawing.Point(562, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // btnComboAdd
+            // 
+            this.btnComboAdd.Location = new System.Drawing.Point(562, 168);
+            this.btnComboAdd.Name = "btnComboAdd";
+            this.btnComboAdd.Size = new System.Drawing.Size(121, 23);
+            this.btnComboAdd.TabIndex = 12;
+            this.btnComboAdd.Text = "Elem hozzáadása";
+            this.btnComboAdd.UseVisualStyleBackColor = true;
+            this.btnComboAdd.Click += new System.EventHandler(this.btnComboAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(561, 203);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(121, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Törlés";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFullDelete
+            // 
+            this.btnFullDelete.Location = new System.Drawing.Point(562, 240);
+            this.btnFullDelete.Name = "btnFullDelete";
+            this.btnFullDelete.Size = new System.Drawing.Size(121, 23);
+            this.btnFullDelete.TabIndex = 14;
+            this.btnFullDelete.Text = "Teljes Törlés";
+            this.btnFullDelete.UseVisualStyleBackColor = true;
+            this.btnFullDelete.Click += new System.EventHandler(this.btnFullDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFullDelete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnComboAdd);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnComboAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFullDelete;
+    }
+}
+
---
+++ Listbox_Gyakorlás/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Listbox_Gyakorlás
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            List<int> Számok = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                Számok.Add(rnd.Next(1000));
+            }
+
+            Számok.Sort();
+
+            for (int i = 0; i < Számok.Count; i++)
+            {
+                listBox1.Items.Add(Számok[i]);
+            }
+            label2.Text ="Elemek száma: " + Convert.ToString(Számok.Count);
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count==0)
+            {
+                MessageBox.Show("A Lista üres");
+            }
+            else
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    if (Convert.ToInt32(listBox1.Items[i])%2==0)
+                    {
+                        listBox2.Items.Add(listBox1.Items[i]);
+                    }
+                }
+                label3.Text = "Elemek száma: " + listBox2.Items.Count.ToString();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!="")
+            {
+                if (!listBox1.Items.Contains(Convert.ToInt32(textBox1.Text)))
+                {
+
+                    if (Convert.ToInt32(textBox1.Text) % 2 == 0)
+                    {
+                        listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+                        listBox2.Items.Add(Convert.ToInt32(textBox1.Text));
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Az elem már szerepel a listában");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Üres mező!");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int keresett =Convert.ToInt32(textBox1.Text);
+            if (listBox1.Items.Contains(keresett))
+            {
+                listBox1.SetSelected(listBox1.Items.IndexOf(keresett), true);
+                if (listBox2.Items.Contains(keresett))
+                {
+                    listBox2.SetSelected(listBox2.Items.IndexOf(keresett), true);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Az elem nem szerepel a listában");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Text = "Balázs Dávid";
+            Button gomb = new Button();
+            gomb.Text = "Kilépés";
+            gomb.Location=new Point(150, 150);
+            gomb.BackColor = Color.Red;
+            gomb.ForeColor = Color.White;
+            this.Controls.Add(gomb);
+            gomb.Click += Gomb_Click;
+        }
+
+        private void Gomb_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = comboBox1.Items.Count.ToString();
+            comboBox1.Items.Add("Balázs Dávid");
+        }
+
+        private void btnComboAdd_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(Interaction.InputBox("Új elem", "Hozzáadás","", 100, 100));
+        }
+
+        private void btnFullDelete_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string törlendő = Interaction.InputBox("Törlendő elem", "törlés", "", 10, 100);
+            if (comboBox1.Items.Contains(törlendő))
+            {
+                comboBox1.Items.Remove(törlendő);
+            }
+            else
+            {
+                MessageBox.Show("Nincs benne az elem");
+            }
+        }
+    }
+}
