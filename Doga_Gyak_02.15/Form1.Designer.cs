aee6df77d55d991af3c57ad507cac78cb177a5a4
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-02-15 17:54:11 +0100

    Add files via upload

---
+++ Doga_Gyak_02.15/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Doga_Gyak_02._15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.txtB_Be = new System.Windows.Forms.TextBox();
+            this.lb_1 = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSorozat = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lb_2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.Location = new System.Drawing.Point(512, 32);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(73, 24);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "label1";
+            // 
+            // txtB_Be
+            // 
+            this.txtB_Be.Location = new System.Drawing.Point(359, 37);
+            this.txtB_Be.Name = "txtB_Be";
+            this.txtB_Be.Size = new System.Drawing.Size(100, 20);
+            this.txtB_Be.TabIndex = 1;
+            this.txtB_Be.TextChanged += new System.EventHandler(this.txtB_Be_TextChanged);
+            // 
+            // lb_1
+            // 
+            this.lb_1.FormattingEnabled = true;
+            this.lb_1.Location = new System.Drawing.Point(359, 73);
+            this.lb_1.Name = "lb_1";
+            this.lb_1.Size = new System.Drawing.Size(120, 199);
+            this.lb_1.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(359, 288);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Feltöltés";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(516, 73);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Rendezés";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(516, 102);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Beszúr";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSorozat
+            // 
+            this.btnSorozat.Location = new System.Drawing.Point(516, 317);
+            this.btnSorozat.Name = "btnSorozat";
+            this.btnSorozat.Size = new System.Drawing.Size(75, 23);
+            this.btnSorozat.TabIndex = 6;
+            this.btnSorozat.Text = "Sorozat";
+            this.btnSorozat.UseVisualStyleBackColor = true;
+            this.btnSorozat.Click += new System.EventHandler(this.btnSorozat_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(668, 155);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Kilépés";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lb_2
+            // 
+            this.lb_2.FormattingEnabled = true;
+            this.lb_2.Location = new System.Drawing.Point(516, 131);
+            this.lb_2.Name = "lb_2";
+            this.lb_2.Size = new System.Drawing.Size(120, 82);
+            this.lb_2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chkBox2);
+            this.panel1.Controls.Add(this.chkBox1);
+            this.panel1.Location = new System.Drawing.Point(516, 250);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(134, 61);
+            this.panel1.TabIndex = 10;
+            // 
+            // chkBox1
+            // 
+            this.chkBox1.AutoSize = true;
+            this.chkBox1.Checked = true;
+            this.chkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkBox1.Location = new System.Drawing.Point(3, 4);
+            this.chkBox1.Name = "chkBox1";
+            this.chkBox1.Size = new System.Drawing.Size(58, 17);
+            this.chkBox1.TabIndex = 0;
+            this.chkBox1.Text = "1-10-ig";
+            this.chkBox1.UseVisualStyleBackColor = true;
+            // 
+            // chkBox2
+            // 
+            this.chkBox2.AutoSize = true;
+            this.chkBox2.Location = new System.Drawing.Point(3, 28);
+            this.chkBox2.Name = "chkBox2";
+            this.chkBox2.Size = new System.Drawing.Size(64, 17);
+            this.chkBox2.TabIndex = 1;
+            this.chkBox2.Text = "11-20-ig";
+            this.chkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Location = new System.Drawing.Point(668, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 76);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Törlés";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Checked = true;
+            this.rb1.Location = new System.Drawing.Point(7, 20);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(104, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Kiválasztott elem";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(7, 44);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(104, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Első elem törlése";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 541);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSorozat);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lb_1);
+            this.Controls.Add(this.txtB_Be);
+            this.Controls.Add(this.lbl_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.TextBox txtB_Be;
+        private System.Windows.Forms.ListBox lb_1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSorozat;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lb_2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox chkBox2;
+        private System.Windows.Forms.CheckBox chkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+    }
+}
+
---
+++ Doga_Gyak_02.15/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doga_Gyak_02._15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_1.Text = "";
+        }
+
+        private void txtB_Be_TextChanged(object sender, EventArgs e)
+        {
+            lbl_1.Text = txtB_Be.Text.ToString();
+            lbl_1.ForeColor = Color.Red;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                lb_1.Items.Add(rnd.Next(10,100));
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lb_1.Sorted = true;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (lbl_1.Text=="")
+            {
+                MessageBox.Show("A mező üres", "Figyelmeztetés");
+            }
+            else
+            {
+                if (lb_2.Items.Contains(lbl_1.Text))
+                {
+                    MessageBox.Show("Az elem már szerepel a listában");
+                }
+                else
+                {
+                    lb_2.Items.Add(lbl_1.Text.ToString());
+                    label2.Text = lb_2.Items.Count.ToString();
+                }
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            if (rb1.Checked)
+            {
+                int selectedIndex=lb_1.SelectedIndex;
+                lb_1.Items.RemoveAt(selectedIndex);
+            }
+            else if (rb2.Checked)
+            {
+                lb_1.Items.RemoveAt(0);
+            }
+            MessageBox.Show($"A lista elemszáma: {lb_1.Items.Count}");
+        }
+
+        private void btnSorozat_Click(object sender, EventArgs e)
+        {
+            if (chkBox1.Checked)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    lb_1.Items.Add(i);
+                }
+            }
+            if (chkBox2.Checked)
+            {
+                for (int i = 11; i <= 20; i++)
+                {
+                    lb_1.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
