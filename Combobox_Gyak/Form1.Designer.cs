ecc3bc6486c8cdfa41efcd877df4d10fa657694f
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-02-16 08:13:38 +0100

    Add files via upload

---
+++ Combobox_Gyak/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Combobox_Gyak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lb_eredm1 = new System.Windows.Forms.ListBox();
+            this.lb_eredm2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adatok";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Feltöltés";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(404, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "...";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(89, 173);
+            this.listBox1.TabIndex = 4;
+            // 
+            // lb_eredm1
+            // 
+            this.lb_eredm1.FormattingEnabled = true;
+            this.lb_eredm1.Location = new System.Drawing.Point(302, 77);
+            this.lb_eredm1.Name = "lb_eredm1";
+            this.lb_eredm1.Size = new System.Drawing.Size(89, 173);
+            this.lb_eredm1.TabIndex = 5;
+            // 
+            // lb_eredm2
+            // 
+            this.lb_eredm2.FormattingEnabled = true;
+            this.lb_eredm2.Location = new System.Drawing.Point(407, 77);
+            this.lb_eredm2.Name = "lb_eredm2";
+            this.lb_eredm2.Size = new System.Drawing.Size(89, 173);
+            this.lb_eredm2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1-20-ig számok",
+            "20 db háromjegyű véletlen szám",
+            "10 db szó",
+            "a-z-ig kisbetűk"});
+            this.comboBox1.Location = new System.Drawing.Point(152, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(152, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(121, 75);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Számok";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Páratlan Számok";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Páros Számok";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(152, 187);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(121, 63);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Szöveg";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(61, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Hossza";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Nagybetűs";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(38, 256);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(89, 23);
+            this.btnLoad.TabIndex = 10;
+            this.btnLoad.Text = "List feltöltése";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(152, 256);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(121, 23);
+            this.btnShow.TabIndex = 11;
+            this.btnShow.Text = "Megjelenítés";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(302, 256);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Kilépés";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lb_eredm2);
+            this.Controls.Add(this.lb_eredm1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lb_eredm1;
+        private System.Windows.Forms.ListBox lb_eredm2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Combobox_Gyak/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Combobox_Gyak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            if (comboBox1.SelectedIndex==0)
+            {
+                listBox1.Items.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    listBox1.Items.Add(Convert.ToInt32(i+1));
+                }
+            }
+            if (comboBox1.SelectedIndex==1)
+            {
+                listBox1.Items.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    listBox1.Items.Add(rnd.Next(100, 1000));
+                }
+            }
+            if (comboBox1.SelectedIndex==2)
+            {
+                string szo = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    szo = Interaction.InputBox($"{i+1}. szó", $"10 db szó", "", 100, 100);
+                    if (listBox1.Items.Contains(szo))
+                    {
+                        if (MessageBox.Show("Ilyen elem már létezik. Hozzáadja?", "Figyelmeztetés", MessageBoxButtons.YesNo)==DialogResult.Yes)
+                        {
+                            listBox1.Items.Add(Convert.ToString(szo));
+                        }
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(Convert.ToString(szo));
+                    }
+                }
+            }
+            if (comboBox1.SelectedIndex==3)
+            {
+                for (char i = 'a'; i <= 'z'; i++)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                lb_eredm1.Items.Clear();
+                lb_eredm1.Visible = true;
+                lb_eredm2.Visible = false;
+                label3.Text = "Páros számok";
+                label3.Visible = true;
+                label4.Visible = false;
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    if (Convert.ToInt32(listBox1.Items[i])%2==0)
+                    {
+                        lb_eredm1.Items.Add(listBox1.Items[i]);
+                    }
+                }
+            }
+            else
+            {
+                lb_eredm2.Items.Clear();
+                lb_eredm2.Visible = true;
+                lb_eredm1.Visible = false;
+                label4.Text = "Páratlan számok";
+                label4.Visible = true;
+                label3.Visible = false;
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    if (Convert.ToInt32(listBox1.Items[i]) % 2 != 0)
+                    {
+                        lb_eredm2.Items.Add(listBox1.Items[i]);
+                    }
+                }
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label3.Text = "Nagybetűs:";
+                label3.Visible = true;
+                lb_eredm1.Items.Clear();
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    lb_eredm1.Items.Add(listBox1.Items[i].ToString().ToUpper());
+                }
+                lb_eredm1.Visible = true;
+            }
+            if (checkBox2.Checked)
+            {
+                label4.Text = "Hossza:";
+                label4.Visible = true;
+                lb_eredm2.Items.Clear();
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    lb_eredm2.Items.Add(Convert.ToInt32(listBox1.Items[i].ToString().Count()));
+                }
+                lb_eredm2.Visible = true;
+            }
+            if (!checkBox1.Checked)
+            {
+                label3.Visible = false;
+                lb_eredm1.Visible = false;
+            }
+            if (!checkBox2.Checked)
+            {
+                label4.Visible = false;
+                lb_eredm2.Visible = false;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
